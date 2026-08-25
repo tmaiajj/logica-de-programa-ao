4ccd13c2b66a8e54fac3d4bdf5f6817f67377b54
tmaiajj <t.maia17122@gmail.com>
2026-08-24 21:41:19 -0300

    exercicios

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let numero = 5;
+let preco = 150 
 
-if (numero > 0) {
-    console.log("numero positivo");
+if (preco > 100) {
+    let desconto = preco * 0.10;
+    let precoFinal = preco - desconto;
+    console.log("Preço final com desconto: " + precoFinal);
+}
+
+let numero1 = 15;
+let numero2 = 10;
+
+if (numero1 > numero2) {
+    console.log(numero1 + " é maior que " + numero2);
+} else if (numero1 < numero2) {
+    console.log(numero2 + " é maior que " + numero1);
 } else {
-    console.log("numero negativo");
+    console.log("Os números são iguais");
+}
+
+let nota = 8.5;
+
+if (nota >= 9){
+    console.log("Excelente");
+} else if (nota >= 7) {
+    console.log("Bom");
+} else {
+    console.log("Precisa melhorar");
 }
